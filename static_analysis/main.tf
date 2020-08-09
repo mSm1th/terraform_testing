eab28dddbdafdf0999051ca850eec7687a40d837
mSm1th <personal_1980@tutanota.com>
2020-08-09 17:20:46 +0100

    updating for static testing

---
+++ static_analysis/main.tf
@@ -0,0 +1,30 @@
+# Create a new instance of the latest Ubuntu 20.04 on an
+# t3.micro node with an AWS Tag naming it "HelloWorld"
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
